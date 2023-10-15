f7be43243c7cdc41a6145410d823375ebde2b518
sangvinjamoor <147975438+sangvinjamoor@users.noreply.github.com>
2023-10-15 15:54:52 +0530

    Add files via upload

---
+++ day1/FirefoxHomeWork.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class FirefoxHomeWork {
+public static void main(String[] args) {
+	float browserVersion = 117.0f;
+	String browserName = "firefox";
+	boolean pageLoaded = true;
+	int noOfBrowser = 1;
+	char browserLogo= 'f';
+	System.out.println("Browser Version " +browserVersion);
+	System.out.println("Browser Name " +browserName);
+	System.out.println("Page Loaded? : " +pageLoaded);
+	System.out.println("Number of Browser: " +noOfBrowser);
+	System.out.println("Browser Logo " +browserLogo);
+}
+}
